90acfcbc03c8af4af4a0cd6fd6b6b490988d11ed
tuacy <1007178106@qq.com>
2018-03-07 18:54:24 +0800

    TabLayout

--- app/src/main/java/com/tuacy/designwidget/module/TextInputLayout/TextInputActivity.java
+++ app/src/main/java/com/tuacy/designwidget/module/TextInputLayout/TextInputActivity.java
@@ -3,8 +3,15 @@ package com.tuacy.designwidget.module.TextInputLayout;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.tuacy.designwidget.R;
 import com.tuacy.designwidget.app.BaseMobileActivity;
@@ -15,7 +22,10 @@ public class TextInputActivity extends BaseMobileActivity {
 		context.startActivity(new Intent(context, TextInputActivity.class));
 	}
 
-	private Toolbar mToolbar;
+	private Toolbar           mToolbar;
+	private TextInputLayout   mTextInputLayout;
+	private TextInputEditText mTextInputEditText;
+	private Button            mButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,9 @@ public class TextInputActivity extends BaseMobileActivity {
 
 	private void initView() {
 		mToolbar = findViewById(R.id.tool_bar);
+		mTextInputLayout = findViewById(R.id.layout_input_password);
+		mTextInputEditText = findViewById(R.id.edit_password);
+		mButton = findViewById(R.id.button_show_error);
 	}
 
 	private void initEvent() {
@@ -35,6 +48,13 @@ public class TextInputActivity extends BaseMobileActivity {
 			@Override
 			public void onClick(View v) {
 				onBackPressed();
+			}
+		});
+		mButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mTextInputLayout.setEnabled(true);
+				mTextInputLayout.setError("密码不能小于6位");
 			}
 		});
 	}
